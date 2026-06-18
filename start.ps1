6c5bfcf24c35b8ef534fe8ba1c999e279fc18a81
Nikhil <nikhilmishi006@gmail.com>
2026-06-18 10:39:37 +0530

    Final fix with production config

---
+++ start.ps1
@@ -0,0 +1,4 @@
+﻿$env:CHATBOT_URL="https://openrouter.ai/api/v1"
+$env:CHATBOT_MODEL="meta-llama/llama-3.2-3b-instruct:free"
+$env:CHATBOT_API_KEY="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"
+node build/app
